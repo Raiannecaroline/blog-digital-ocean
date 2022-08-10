7b175374757271561e8dbff1fd0ac6bd893f229b
Raiane Caroline Souza Teixeira Ferreira <raianecaroline.st@gmail.com>
2022-08-10 15:05:45 +0000

    index.html

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Hello")
 
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def usuario():
+    usuario = [1, "Raiane Caroline ", "Aluna"]
+    alunos = ["Emanoel", "Elvis", "Fernando", "Raiane"]
+    return render_template("index.html", usuario=usuario, alunos=alunos)
+
+@app.route("/contatos")
+def contato():
+    nomeAula = "Aula python para Web"
+    return render_template("index.html", nome=nomeAula)
